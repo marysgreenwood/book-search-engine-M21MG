a46bca54c5084389ef2606883b653ca9f1981928
marysgreenwood <marysgreenwood@gmail.com>
2023-03-15 23:52:27 -0400

    something fixed it

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "..utils/mutations";
+import { REMOVE_BOOK } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
